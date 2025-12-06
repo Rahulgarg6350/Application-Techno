d0dabd053776089a3730b4bb3e31bd60a46da3e9
Rahul Garg <92792294+Rahulgarg6350@users.noreply.github.com>
2025-12-06 20:53:09 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          blue: '#072b66',
+          yellow: '#ffd400',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
